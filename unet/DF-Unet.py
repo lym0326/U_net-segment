18fdc9148373a7887fce7cece643ac7a365bbf6f
lym0326 <132065945+lym0326@users.noreply.github.com>
2023-07-11 18:09:08 +0800

    Create DF-Unet.py

---
+++ unet/DF-Unet.py
@@ -0,0 +1,224 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+__all__ = ['UNet', 'NestedUNet']
+
+class SelFuseFeature(nn.Module):
+    def __init__(self, in_channels, shift_n=5, n_class=4, auxseg=False):
+        super(SelFuseFeature, self).__init__()
+
+        self.shift_n = shift_n
+        self.n_class = n_class
+        self.auxseg = auxseg
+        self.fuse_conv = nn.Sequential(nn.Conv2d(in_channels * 2, in_channels, kernel_size=1, padding=0),
+                                       nn.BatchNorm2d(in_channels),
+                                       nn.ReLU(inplace=True),
+                                       )
+        if auxseg:
+            self.auxseg_conv = nn.Conv2d(in_channels, self.n_class, 1)
+
+    def forward(self, x, df):
+        N, _, H, W = df.shape
+        mag = torch.sqrt(torch.sum(df ** 2, dim=1))
+        greater_mask = mag > 0.5
+        greater_mask = torch.stack([greater_mask, greater_mask], dim=1)
+        df[~greater_mask] = 0
+
+        scale = 1.
+
+        grid = torch.stack(torch.meshgrid(torch.arange(H), torch.arange(W)), dim=0)
+        grid = grid.expand(N, -1, -1, -1).to(x.device, dtype=torch.float).requires_grad_()
+        grid = grid + scale * df
+
+        grid = grid.permute(0, 2, 3, 1).transpose(1, 2)
+        grid_ = grid + 0.
+        grid[..., 0] = 2 * grid_[..., 0] / (H - 1) - 1
+        grid[..., 1] = 2 * grid_[..., 1] / (W - 1) - 1
+
+        # features = []
+        select_x = x.clone()
+        for _ in range(self.shift_n):
+            select_x = F.grid_sample(select_x, grid, mode='bilinear', padding_mode='border')
+            # features.append(select_x)
+        # select_x = torch.mean(torch.stack(features, dim=0), dim=0)
+        # features.append(select_x.detach().cpu().numpy())
+        # np.save("/root/chengfeng/Cardiac/source_code/logs/acdc_logs/logs_temp/feature.npy", np.array(features))
+        if self.auxseg:
+            auxseg = self.auxseg_conv(x)
+        else:
+            auxseg = None
+
+        select_x = self.fuse_conv(torch.cat([x, select_x], dim=1))
+        return [select_x, auxseg]
+
+
+class VGGBlock(nn.Module):
+    def __init__(self, n_channels, middle_channels, out_channels):
+        super().__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(n_channels, middle_channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(middle_channels)
+        self.conv2 = nn.Conv2d(middle_channels, out_channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        return out
+
+
+class UNet(nn.Module):
+    def __init__(self, n_classes, n_channels=3, **kwargs):
+        super().__init__()
+
+        nb_filter = [32, 64, 128, 256, 512]
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)#scale_factor:放大的倍数  插值
+
+        self.conv0_0 = VGGBlock(n_channels, nb_filter[0], nb_filter[0])
+        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
+        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
+        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
+        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
+
+        self.conv3_1 = VGGBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
+        self.conv2_2 = VGGBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
+        self.conv1_3 = VGGBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv0_4 = VGGBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
+
+        self.final = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+
+
+    def forward(self, input):
+        x0_0 = self.conv0_0(input)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x4_0 = self.conv4_0(self.pool(x3_0))
+
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, self.up(x3_1)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, self.up(x2_2)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, self.up(x1_3)], 1))
+
+        output = self.final(x0_4)
+        return output
+
+
+class DF-Unet(nn.Module):
+    def __init__(self, n_classes=4, n_channels=3, deep_supervision=False, bilinear=False,**kwargs):
+        super().__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        nb_filter = [32, 64, 128, 256, 512]
+        self.bilinear = bilinear
+        self.deep_supervision = deep_supervision
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv0_0 = VGGBlock(n_channels, nb_filter[0], nb_filter[0])
+        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
+        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
+        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
+        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
+
+        self.conv0_1 = VGGBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_1 = VGGBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv2_1 = VGGBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
+        self.conv3_1 = VGGBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
+
+        self.conv0_2 = VGGBlock(nb_filter[0]*2+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_2 = VGGBlock(nb_filter[1]*2+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv2_2 = VGGBlock(nb_filter[2]*2+nb_filter[3], nb_filter[2], nb_filter[2])
+
+        self.conv0_3 = VGGBlock(nb_filter[0]*3+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_3 = VGGBlock(nb_filter[1]*3+nb_filter[2], nb_filter[1], nb_filter[1])
+
+        self.conv0_4 = VGGBlock(nb_filter[0]*4+nb_filter[1], nb_filter[0], nb_filter[0])
+
+        if self.deep_supervision:
+            self.final1 = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+            self.final2 = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+            self.final3 = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+            self.final4 = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+        else:
+            self.final = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+
+        # Direct Field
+        shift_n = 1
+        auxseg = True
+        selfeat = True
+        num_class = 4
+        self.ConvDf_1x1 = nn.Conv2d(32, 2, kernel_size=1, stride=1, padding=0)
+
+        if selfeat:
+            self.SelDF = SelFuseFeature(32, auxseg=auxseg, shift_n=shift_n)
+
+        #self.Conv_1x1 = nn.Conv2d(32, num_class, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self, input):
+
+        x0_0 = self.conv0_0(input)
+
+        x1_0 = self.conv1_0(self.pool(x0_0))
+
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
+
+
+        x2_0 = self.conv2_0(self.pool(x1_0))
+
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
+
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
+
+
+        x3_0 = self.conv3_0(self.pool(x2_0))
+
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
+
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
+
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
+
+        x4_0 = self.conv4_0(self.pool(x3_0))
+
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
+
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
+
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
+
+        # df = None
+        df = self.ConvDf_1x1(x0_4)
+        if True:
+            d2_auxseg = self.SelDF(x0_4, df)
+            x0_4, auxseg = d2_auxseg[:2]
+            print(type(x0_4))
+        else:
+            auxseg = None
+
+
+        if self.deep_supervision:
+            output1 = self.final1(x0_1)
+            output2 = self.final2(x0_2)
+            output3 = self.final3(x0_3)
+            output4 = self.final4(x0_4)
+
+
+            return [output1, output2, output3, output4]
+
+        else:
+            output = self.final(x0_4)
+            return output
